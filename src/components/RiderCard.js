fa43c9f2f66c3366ab8afc3c73f2f0d72e57373e
Derek Rogers <derekgrogers1@gmail.com>
2022-07-26 17:56:34 -0500

    Updated rider cards so attribute symobls appear next to the number

--- src/components/RiderCard.js
+++ src/components/RiderCard.js
@@ -1,5 +1,11 @@
-import flat_symbol from '../rider_card_images/flat_symbol.jpg';
-//import hill_symbol from '../rider_card_images/hill_symbol.jpg';
+import flat_symbol from '../rider_card_images/flat_symbol.png';
+import hill_symbol from '../rider_card_images/hill_symbol.png';
+import mountain_symbol from '../rider_card_images/mountain_symbol.png';
+import downhill_symbol from '../rider_card_images/downhill_symbol.png';
+import sprint_symbol from '../rider_card_images/sprint_symbol.png';
+import breakaway_symbol from '../rider_card_images/breakaway_symbol.png';
+import cobbleStone_symbol from '../rider_card_images/cobblestone_symbol.png';
+import wind_symbol from '../rider_card_images/wind_symbol.png';
 
 const RiderCard = ({rider}) => {
   return (
@@ -14,19 +20,37 @@ const RiderCard = ({rider}) => {
                         {rider.flat}
                     </div>
                     <div>
-                        <img src="../rider_card_images/hill_symbol.jpg" alt="hill symbol:"/>
+                        <img src={hill_symbol} alt="hill symbol:"/>
                         {rider.hill}
                     </div>
-                    <div>{rider.mountain}</div>
-                    <div>{rider.downhill}</div>
+                    <div>
+                        <img src={mountain_symbol} alt="mountain symbol:"/>
+                        {rider.mountain}
+                    </div>
+                    <div>
+                        <img src={downhill_symbol} alt="downhill_symbol symbol:"/>
+                        {rider.downhill}
+                    </div>
                 </div>
                 <div style={EventModifiers}>
-                    <div>{rider.sprint}</div>
-                    <div>{rider.breakaway}</div>
+                    <div>
+                        <img src={sprint_symbol} alt="sprint_symbol symbol:"/>
+                        {rider.sprint}
+                    </div>
+                    <div>
+                        <img src={breakaway_symbol} alt="breakaway_symbol symbol:"/>
+                        {rider.breakaway}
+                    </div>
                 </div>
                 <div style={ConditionModifiers}>
-                    <div>{rider.cobbleStone}</div>
-                    <div>{rider.wind}</div>
+                    <div>
+                        <img src={cobbleStone_symbol} alt="cobbleStone_symbol symbol:"/>
+                        {rider.cobbleStone}
+                    </div>
+                    <div>
+                        <img src={wind_symbol} alt="wind_symbol symbol:"/>
+                        {rider.wind}
+                    </div>
                 </div>
             </div>
         </div>
@@ -38,7 +62,10 @@ const RiderCardStyle = {
     backgroundColor: 'yellow', 
     color: 'red',
     width: '450px',
-    height: '600px'
+    height: '600px',
+    border: '15px solid green',
+    padding: '50px',
+    margin: '20px'
 }
 
 const RiderNameStyle = {
